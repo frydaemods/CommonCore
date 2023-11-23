76e5aa7435a593521b16940f398eace8766f2927
MrFrydae <MrFrydae@gmail.com>
2023-11-23 11:29:33 -0500

    Move utility commands to test server

--- src/main/java/dev/frydae/commoncore/CommonCore.java
+++ src/main/java/dev/frydae/commoncore/CommonCore.java
@@ -1,11 +1,10 @@
 package dev.frydae.commoncore;
 
-import dev.frydae.commoncore.commands.Commands;
 import net.fabricmc.api.DedicatedServerModInitializer;
 
 public class CommonCore implements DedicatedServerModInitializer {
     @Override
     public void onInitializeServer() {
-        Commands.registerCommands();
+
     }
 }
