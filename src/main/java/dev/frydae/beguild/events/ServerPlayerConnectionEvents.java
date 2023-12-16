d283e115adc07d81d22f46705e9cdbe7c5220724
MrFrydae <MrFrydae@gmail.com>
2023-12-16 06:53:30 -0500

    cleanup

--- src/main/java/dev/frydae/beguild/events/ServerPlayerConnectionEvents.java
+++ src/main/java/dev/frydae/beguild/events/ServerPlayerConnectionEvents.java
@@ -1,53 +1,13 @@
 package dev.frydae.beguild.events;
 
-import dev.frydae.beguild.systems.LanguageLoader;
-import dev.frydae.beguild.user.RegisteredUser;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
 import java.util.Collections;
 import java.util.List;
 
 public final class ServerPlayerConnectionEvents {
-
-    public static Event<DisconnectMessageCallback> DISCONNECT_MESSAGE = new Event<>(DisconnectMessageCallback.class, (listeners) -> (user) -> {
-        Text response = Text.literal(LanguageLoader.getString("multiplayer.player.left", user.getName())).formatted(Formatting.YELLOW);
-        boolean responseSet = false;
-
-        for (DisconnectMessageCallback listener : listeners) {
-            Text hold = listener.onDisconnectMessage(user);
-
-            if (!responseSet && hold != null) {
-                response = hold;
-
-                responseSet = true;
-            }
-        }
-
-        return response;
-    });
-
-    public static Event<JoinMessageCallback> JOIN_MESSAGE = new Event<>(JoinMessageCallback.class, (listeners) -> (user) -> {
-        Text response = Text.literal(LanguageLoader.getString("multiplayer.player.joined", user.getName())).formatted(Formatting.YELLOW);
-
-        boolean responseSet = false;
-
-        for (JoinMessageCallback listener : listeners) {
-            Text hold = listener.onJoinMessage(user);
-
-            if (!responseSet && hold != null) {
-                response = hold;
-
-                responseSet = true;
-            }
-        }
-
-        return response;
-    });
-
     public static Event<SpawnPacketFilterCallback> SPAWN_PACKET_FILTER = new Event<>(SpawnPacketFilterCallback.class, (listeners) -> (packet, target) -> {
         for (SpawnPacketFilterCallback listener : listeners) {
             if (!listener.onSpawnPacketSend(packet, target)) {
@@ -69,16 +29,6 @@ public final class ServerPlayerConnectionEvents {
 
         return playersToSendTo;
     });
-
-    @FunctionalInterface
-    public interface DisconnectMessageCallback {
-        Text onDisconnectMessage(RegisteredUser user) throws InterruptedException;
-    }
-
-    @FunctionalInterface
-    public interface JoinMessageCallback {
-        Text onJoinMessage(RegisteredUser user) throws InterruptedException;
-    }
 
     @FunctionalInterface
     public interface SpawnPacketFilterCallback {
