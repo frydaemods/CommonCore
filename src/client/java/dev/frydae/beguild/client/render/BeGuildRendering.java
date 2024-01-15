944e9ff1ccda18e64663a43cb4c42cdf5d095655
MrFrydae <MrFrydae@gmail.com>
2024-01-15 10:51:42 -0500

    Verify textures/sprites exist before using

--- src/client/java/dev/frydae/beguild/client/render/BeGuildRendering.java
+++ src/client/java/dev/frydae/beguild/client/render/BeGuildRendering.java
@@ -1,13 +1,20 @@
 package dev.frydae.beguild.client.render;
 
+import com.google.common.collect.Maps;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.texture.AbstractTexture;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.util.Identifier;
 
+import java.util.Map;
+
 @Environment(EnvType.CLIENT)
 public final class BeGuildRendering {
+    private static final Map<Identifier, Boolean> TEXTURE_EXISTENCE_CACHE = Maps.newConcurrentMap();
+    private static final Map<Identifier, Boolean> SPRITE_EXISTENCE_CACHE = Maps.newConcurrentMap();
+
     private BeGuildRendering() {}
 
     public static boolean isDarkMode() {
@@ -16,16 +23,36 @@ public final class BeGuildRendering {
     }
 
     private static boolean verifySpriteExists(Identifier id) {
-        Sprite sprite = MinecraftClient.getInstance().getGuiAtlasManager().getSprite(id);
+        if (!SPRITE_EXISTENCE_CACHE.containsKey(id)) {
+            Sprite sprite = MinecraftClient.getInstance().getGuiAtlasManager().getSprite(id);
 
-        return sprite != null;
+            SPRITE_EXISTENCE_CACHE.put(id, sprite != null);
+        }
+
+        return SPRITE_EXISTENCE_CACHE.get(id);
+    }
+
+    private static boolean verifyTextureExists(Identifier id) {
+        if (!TEXTURE_EXISTENCE_CACHE.containsKey(id)) {
+            AbstractTexture texture = MinecraftClient.getInstance().getTextureManager().getTexture(id);
+
+            TEXTURE_EXISTENCE_CACHE.put(id, texture != null);
+        }
+
+        return TEXTURE_EXISTENCE_CACHE.get(id);
+    }
+
+    private static String alterDarkModePath(String path) {
+        return path.substring(0, path.lastIndexOf("/")) + "/dark" + path.substring(path.lastIndexOf("/"));
     }
 
     public static Identifier processUITexture(Identifier original) {
         if (isDarkMode()) {
-            String path = original.getPath();
+            Identifier darkTexture = new Identifier("beguild", alterDarkModePath(original.getPath()));
 
-            return new Identifier("beguild", path.substring(0, path.lastIndexOf("/")) + "/dark" + path.substring(path.lastIndexOf("/")));
+            if (verifyTextureExists(darkTexture)) {
+                return darkTexture;
+            }
         }
 
         return original;
@@ -33,9 +60,7 @@ public final class BeGuildRendering {
 
     public static Identifier processSpriteTexture(Identifier original) {
         if (isDarkMode()) {
-            String path = original.getPath();
-
-            Identifier darkSprite = new Identifier(path.substring(0, path.lastIndexOf("/")) + "/dark" + path.substring(path.lastIndexOf("/")));
+            Identifier darkSprite = new Identifier(alterDarkModePath(original.getPath()));
 
             if (verifySpriteExists(darkSprite)) {
                 return darkSprite;
