7d7f340ebb480f939992047e7d6f16e95986ccb5
MrFrydae <MrFrydae@gmail.com>
2024-08-17 15:52:38 -0400

    Moved a lot of common code to Core

--- src/main/java/dev/frydae/beguild/BeGuildCommon.java
+++ src/main/java/dev/frydae/beguild/BeGuildCommon.java
@@ -1,108 +1,11 @@
 package dev.frydae.beguild;
 
-import co.aikar.taskchain.TaskChain;
-import co.aikar.taskchain.TaskChainFactory;
-import com.mojang.logging.LogUtils;
-import dev.frydae.beguild.loader.BeGuildMod;
 import dev.frydae.beguild.screens.BeGuildScreenHandlerType;
-import dev.frydae.beguild.user.RegisteredUser;
-import dev.frydae.beguild.user.UserManager;
-import dev.frydae.beguild.utils.taskchain.FabricTaskChainFactory;
-import dev.frydae.commands.FabricCommandCompletions;
-import dev.frydae.commands.FabricCommandConditions;
-import dev.frydae.commands.FabricCommandContexts;
-import dev.frydae.commands.IllegalCommandException;
-import lombok.Getter;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.minecraft.server.MinecraftServer;
-import org.slf4j.Logger;
+import net.fabricmc.api.DedicatedServerModInitializer;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-
-public class BeGuildCommon extends BeGuildMod {
-    @Getter private static ScheduledExecutorService scheduler;
-    @Getter private static Logger logger = LogUtils.getLogger();
-
-    @Getter private static MinecraftServer server;
-    @Getter private static TaskChainFactory taskChainFactory;
-
+public class BeGuildCommon implements DedicatedServerModInitializer {
     @Override
-    public void registerCommands(FabricCommandCompletions commandCompletions, FabricCommandContexts commandContexts, FabricCommandConditions commandConditions) {
-        registerContexts(commandContexts);
-        registerCompletions(commandCompletions);
-    }
-
-    private static void registerContexts(FabricCommandContexts commandContexts) {
-        commandContexts.registerContext(RegisteredUser.class, c -> {
-            RegisteredUser user = UserManager.getUser(c.getParameterText());
-
-            if (user == null) {
-                throw new IllegalCommandException("User: " + c.getParameterText() + " cannot be found.");
-            }
-
-            return user;
-        });
-    }
-
-    private static void registerCompletions(FabricCommandCompletions commandCompletions) {
-        commandCompletions.registerSmartCompletion("allplayers", c -> {
-            Integer lastDays = null;
-
-            for (Map.Entry<String, String> stringStringEntry : c.getConfigs().entrySet()) {
-                try {
-                    lastDays = Integer.parseInt(stringStringEntry.getKey());
-                } catch (NumberFormatException ignored) {}
-            }
-
-            List<String> users = UserManager.getUsers(lastDays);
-
-            if (c.hasConfig("other")) {
-                users.remove(c.getContext().getSource().getName());
-            }
-
-            return users;
-        });
-    }
-
-    @Override
-    public void onInitialize() {
-        scheduler = Executors.newScheduledThreadPool(1);
-
+    public void onInitializeServer() {
         BeGuildScreenHandlerType.dummy();
     }
-
-    @Override
-    public void earliestStarting(MinecraftServer server) {
-        BeGuildCommon.server = server;
-    }
-
-    @Override
-    public void onStarting(MinecraftServer server) {
-        ServerPlayConnectionEvents.JOIN.register((handler, sender, server1) -> {
-            UserManager.addOrUpdatePlayer(handler.getPlayer());
-        });
-
-        taskChainFactory = FabricTaskChainFactory.create(server);
-    }
-
-    @Override
-    public void onStarted(MinecraftServer server) {
-        UserManager.get();
-    }
-
-    @Override
-    public void onStopping(MinecraftServer server) {
-        scheduler.shutdownNow();
-    }
-
-    public static <T> TaskChain<T> newChain() {
-        return taskChainFactory.newChain();
-    }
-    public static <T> TaskChain<T> newSharedChain(String name) {
-        return taskChainFactory.newSharedChain(name);
-    }
-
 }
--- src/main/java/dev/frydae/beguild/interfaces/ListeningInventory.java
+++
@@ -1,9 +0,0 @@
-package dev.frydae.beguild.interfaces;
-
-import net.minecraft.inventory.InventoryChangedListener;
-
-public interface ListeningInventory {
-    void beguild$addListener(InventoryChangedListener listener);
-
-    void beguild$removeListener(InventoryChangedListener listener);
-}
