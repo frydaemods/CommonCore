24ebabbb8867da15853bafc091070a813826249e
MrFrydae <MrFrydae@gmail.com>
2024-08-26 11:52:43 -0400

    Remove cmd system as unused

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,18 +15,6 @@ dependencies {
     mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
     modImplementation("net.fabricmc:fabric-loader:${property("fabric_loader_version")}")
     modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
-
-    modImplementation("dev.frydae:fcs-fabric:${version}")?.let { include(it) }
-}
-
-repositories {
-    maven { url = uri("https://maven.isxander.dev/releases") }
-    maven { url = uri("https://maven.isxander.dev/snapshots") }
-    maven { url = uri("https://maven.nucleoid.xyz/") }
-    maven { url = uri("https://maven.enginehub.org/repo") }
-    maven { url = uri("https://maven.frydae.dev/releases/") }
-    maven { url = uri("https://maven.frydae.dev/snapshots/") }
-    maven { url = uri("https://ci.emc.gs/nexus/content/groups/aikar/") }
 }
 
 java {
